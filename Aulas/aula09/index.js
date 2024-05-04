8ed8f8b91da5f55b254523e834e66edba9cb73a5
Júlia B <julia.b.silva@iesb.edu.br>
2024-05-03 21:39:22 -0300

    aula 9

--- Aulas/aula09/index.js
+++ Aulas/aula09/index.js
@@ -1,16 +1,75 @@
 require('dotenv').config(); //carregando todas as constantes/variaveis do env na memoria
-
 const mongoose = require('mongoose');
+const Produto = require('./modelo');
 
 const url = process.env.MONGODB_URL; //joga tudo que o config salva na memoria na url 
 
 async function main(){
     try{
-        await mongoose.connect();
+        await mongoose.connect(url);
         console.log("De boas");
     } catch(erro){
         console.log("Deu ruim!", erro.message);
     }
+
+
+    //inserir um produto
+    const produto = new Produto({
+        nome: "  uv        ",
+        preco: -2,
+    });
+    try{
+        await produto.save();
+        console.log(produto);
+    } catch (err){
+        for(let key in err.errors){
+            console.log(err.errors[key].message);
+        }
+        //err.error.forEach(erro => console.log(erro.message));
+    }
+    
+
+    /*const produto = await Produto.create({
+        nome: "uva",
+        preco: 32.5,
+        quantidade: 10
+    });
+    console.log(produto);*/
+
+    //inserir N produtos
+    /*const produtos = await Produto.insertMany([
+        {nome: "maca", preco: 20.7, quantidade: 8},
+        {nome: "pera", preco: 12.5, quantidade: 18},
+        {nome: "laranja", preco: 25.6, quantidade: 28},
+    ]);
+    console.log(produtos);*/
+
+    //consultar 1 produto
+    //const produto = await Produto.findOne({nome: "ameixa"});
+    //console.log(produto);
+
+    //consultar N produtos
+    //const produto = await Produto.find({nome: "banana"});
+    // console.log(produto );
+
+    //atualizar 1 produto
+    // const produto = await Produto.findOneAndUpdate(
+    //     {nome: "banana"}, {nome: "banana nanica", preco: 15.0, quantidade: 20}
+    // );
+    // console.log(produto);
+    // const result = await Produto.updateOne(
+    //     {nome:"banana"}, 
+    //     {nome: "banana nanica", preco: 15.0, quantidade: 20}
+    //     );
+    //     console.log(result);
+
+    //remover 1 produto
+    // const produto = await Produto.findOneAndDelete({nome: "uva"});
+    // console.log(produto);
+
+    //remover varios = deleteMany
+
+
     await mongoose.disconnect(); //desconecta se a aplicação cair
 };
 
--- Aulas/aula09/modelo.js
+++ Aulas/aula09/modelo.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 // criar esquema(estrutura) pra salvar o documento no banco de dados
 
 const produtoSchema = new mongoose.Schema({
-    nome: String, 
-    preco: Number,
-    quantidade: Number
+    nome: {
+        type: String, 
+        trim: true, 
+        uppercase: true, 
+        required: [true, "Nome é obrigatorio"], 
+        minLength:[3, "Nome deve conter no minimo 3 letras"]}, 
+    //função trim vai aplicar no nome e tira os espaços em brancos no final
+    // required = obrigatorio 
+    preco: {type: Number, min: 0},
+    quantidade: {type: Number, default:0}
 });
 
 module.exports = mongoose.model('Produto', produtoSchema); //nome sempre no singular

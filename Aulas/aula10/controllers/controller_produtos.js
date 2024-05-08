efde7a069648e3cb169c3bf54b28e50c2534704f
Júlia B <julia.b.silva@iesb.edu.br>
2024-05-07 21:50:24 -0300

    Aula 10

---
+++ Aulas/aula10/controllers/controller_produtos.js
@@ -0,0 +1,19 @@
+const Produto = require('../models/model_produtos');
+
+async function validarDados(req, res, next){
+    const produto = new Produto(req.body);
+    try{
+        await produto.validate();
+        next();
+    } catch (err){
+        res.status(422).json({msg: "Dados do produto inválidos"});
+    }
+
+};
+
+async function criar(req, res){
+    const produto = await Produto.create(req.body);
+    res.status(201).json({});
+};
+
+module.exports = {validarDados, criar};
---
+++ Aulas/aula10/tests/router_produtos.test.js
@@ -0,0 +1,14 @@
+const supertest = require('supertest');
+const app = require('../app');
+const request = supertest(app);
+
+let id = null;
+
+describe('API Loja Virtual', () =>{
+    test('Deve retornar 201 e um JSON no POST /produtos', async () =>{
+        const response = await request.post('/produtos').send({nome: "banana", preco: 12.0});
+        expect(response.status).toBe(201);
+        expect(response.type).toBe('application/json')
+        id = response.body._id;
+    });
+});
